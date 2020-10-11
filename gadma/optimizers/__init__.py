a107c943f2dd8992fd703df1d343844a278cdcd5
Ekaterina <katenos823@gmail.com>
2020-10-11 13:33:51 +0300

    comment out old class and check bounds in constrain

--- gadma/optimizers/__init__.py
+++ gadma/optimizers/__init__.py
@@ -4,4 +4,4 @@ from .global_optimizer import *
 from .genetic_algorithm import *
 from .bayesian_optimization import *
 from .combinations import *
-from .linear_constrain import LinearConstrain, LinearConstrainDemographics
+from .linear_constrain import LinearConstrain
