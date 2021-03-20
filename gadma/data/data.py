fcb0b3f269de93bd6c42f24d7fd061fde84741cd
Ekaterina <katenos823@gmail.com>
2021-03-20 20:59:39 +0300

    update VCF format

--- gadma/data/data.py
+++ gadma/data/data.py
@@ -1,5 +1,5 @@
 from ..utils import check_file_existence, ensure_file_existence
-
+from ..utils import read_popinfo, get_list_of_names_from_vcf
 
 class DataHolder(object):
     """
@@ -39,15 +39,45 @@ class SFSDataHolder(DataHolder):
 class VCFDataHolder(DataHolder):
     """
     Class for VCF data holding.
+    It saves some information while it is created.
     """
-    def __init__(self, vcf_file, popmap_file, sample_sizes, outgroup,
-                 population_labels=None, seq_len=None,  bed_file=None):
+    def __init__(self, vcf_file, popmap_file, sample_sizes=None, outgroup=None, ploidy=2,
+                 population_labels=None, seq_len=None,  bed_file=None,
+                 reference_file=None):
+        sample2pop = read_popinfo(popmap_file)
+        samples = get_list_of_names_from_vcf(vcf_file)
+        wrong_samples = list()
+        for sample in sample2pop:
+            if sample not in samples:
+                wrong_samples.append(sample)
+        if len(wrong_samples) > 0:
+            warnings.warn(f"Some samples from popmap file are not presented "
+                          f"in VCF file: {wrong_samples}. They were ignored.")
+            for sample in wrong_samples:
+                del sample2pop[sample]
+        # Check pop labels
+        pop_labels = list()
+        for sample in samples:
+            if sample in sample2pop:
+                if sample2pop[sample] not in pop_labels:
+                    pop_labels.append(sample2pop[sample])
         if population_labels is None:
-            population_labels = set()
-            with open(popmap_file) as f:
-                for line in f:
-                    population_labels.add(line.split()[-1])
+            population_labels = pop_labels
+        if set(population_labels) != set(pop_labels):
+            raise AssertionError("Population labels are different in popmap "
+                                 f"file: {population_labels} != {pop_labels}")
+
+        # Check sample sizes
+        sizes = [sum([x == pop for x in sample2pop.values()])
+                 for pop in population_labels]
+        sizes *= ploidy
+        if sample_sizes is None:
+            sample_sizes = sizes
+        assert sample_sizes == sizes, ("Sizes are not equal: "
+                                       f"{sample_sizes} != {sizes}")
         super(VCFDataHolder, self).__init__(vcf_file, sample_sizes, outgroup,
                                             population_labels, seq_len)
         self.popmap_file = popmap_file
         self.bed_file = bed_file
+        self.ploidy = ploidy
+        self.reference_file = reference_file
