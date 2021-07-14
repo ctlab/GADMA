da77d5a0aee07a613708b61141374994a128d592
Ekaterina <ekaterina.e.noskova@gmail.com>
2021-07-15 00:39:56 +0300

    Add more functions for data_utils and call them to update data_holder

--- gadma/data/__init__.py
+++ gadma/data/__init__.py
@@ -1,3 +1,5 @@
 from .data import DataHolder, SFSDataHolder, VCFDataHolder  # NOQA
 from .data_utils import get_defaults_from_vcf_format, ploidy_from_vcf  # NOQA
 from .data_utils import check_population_labels_vcf, check_projections_vcf  # NOQA
+from .data_utils import check_and_return_projections_and_labels  # NOQA
+from .data_utils import update_data_holder_with_inner_data  # NOQA
