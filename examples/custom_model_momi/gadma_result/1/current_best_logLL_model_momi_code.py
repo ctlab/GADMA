8a541ddbd5ab4747bd729bb2160f05de948cd619
Ekaterina <ekaterina.e.noskova@gmail.com>
2021-09-03 20:50:56 +0300

    Change example

--- examples/custom_model_momi/gadma_result/1/current_best_logLL_model_momi_code.py
+++ examples/custom_model_momi/gadma_result/1/current_best_logLL_model_momi_code.py
@@ -1,21 +1,22 @@
 import momi
 import importlib.util
 
-spec = importlib.util.spec_from_file_location('module', '/home/katenos/Workspace/popgen/temp/GADMA/examples/custom_model_momi/demographic_model.py')
+spec = importlib.util.spec_from_file_location('module', '../../demographic_model.py')
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 model_func = module.model_func
 
 
 # Momi does not supports downsizing of the SFS so in this code there is no downsizing.
-data = momi.sfs_from_dadi('/home/katenos/Workspace/popgen/temp/GADMA/examples/custom_model_momi/YRI_CEU.fs')
-data = data.subset_populations(['YRI', 'CEU'])
+ind2pop = {'tsk_0': 'YRI', 'tsk_1': 'YRI', 'tsk_2': 'CEU', 'tsk_3': 'CEU', 'tsk_4': 'CHB', 'tsk_5': 'CHB'}
+data = momi.SnpAlleleCounts.read_vcf('../../out_of_africa_chr22_sim.vcf', ind2pop=ind2pop).extract_sfs(n_blocks=100)
+data = data.subset_populations(['YRI', 'CEU', 'CHB'])
 
-params = [109.36454428269073, 256330.26580715246, 0.0008948579139255634, 4.7288093065137735, 1402947.4995631143, 159.1072306368505]
+params = [7320.667634088726, 8819.883079004429, 7340.677704023327, 0.10090026131353586, 0.001, 3287.381754159632, -6.633272527137954e-05, 4398.863616512322, 2490.0807037094955, 8.880592874940543]
 model = model_func(params)
 model.gen_time = 1
-model.muts_per_gen = 2.35e-08
-model.set_data(data, length=4040000)
+model.muts_per_gen = 1.29e-08
+model.set_data(data, length=51304566)
 ll_model = model.log_likelihood()
 print(f'Value of log-likelihood: {ll_model}')
 from matplotlib import pyplot as plt
--- examples/custom_model_momi/gadma_result/2/current_best_logLL_model_momi_code.py
+++ examples/custom_model_momi/gadma_result/2/current_best_logLL_model_momi_code.py
@@ -1,21 +1,22 @@
 import momi
 import importlib.util
 
-spec = importlib.util.spec_from_file_location('module', '/home/katenos/Workspace/popgen/temp/GADMA/examples/custom_model_momi/demographic_model.py')
+spec = importlib.util.spec_from_file_location('module', '../../demographic_model.py')
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 model_func = module.model_func
 
 
 # Momi does not supports downsizing of the SFS so in this code there is no downsizing.
-data = momi.sfs_from_dadi('/home/katenos/Workspace/popgen/temp/GADMA/examples/custom_model_momi/YRI_CEU.fs')
-data = data.subset_populations(['YRI', 'CEU'])
+ind2pop = {'tsk_0': 'YRI', 'tsk_1': 'YRI', 'tsk_2': 'CEU', 'tsk_3': 'CEU', 'tsk_4': 'CHB', 'tsk_5': 'CHB'}
+data = momi.SnpAlleleCounts.read_vcf('../../out_of_africa_chr22_sim.vcf', ind2pop=ind2pop).extract_sfs(n_blocks=100)
+data = data.subset_populations(['YRI', 'CEU', 'CHB'])
 
-params = [102726.06351515892, 59543.758923395275, 0.001, 3.986264525061683, 7929.578722814034, 5272.715387420992]
+params = [15004.596619670217, 7223.225653867972, 270.24291209230444, 0.0690831942210091, 0.0009024496699107315, 1820.3282780957186, -0.000874306156876506, 360037.9623043344, 2424.316004053876, 36.219376382869015]
 model = model_func(params)
 model.gen_time = 1
-model.muts_per_gen = 2.35e-08
-model.set_data(data, length=4040000)
+model.muts_per_gen = 1.29e-08
+model.set_data(data, length=51304566)
 ll_model = model.log_likelihood()
 print(f'Value of log-likelihood: {ll_model}')
 from matplotlib import pyplot as plt
