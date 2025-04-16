cee3dd7ad442d003e13a92ed2fe0c28159a70afa
noscode <ekaterina.e.noskova@gmail.com>
2025-04-16 17:17:15 +0100

    Fix issue with version

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     # Set the name so that github correctly tracks reverse dependencies.
     # https://github.com/ctlab/GADMA/network/dependents
     name="gadma",
-    use_scm_version={"write_to": "gadma/_version.py"},
+    use_scm_version={"write_to": "gadma/version.py"},
 )
